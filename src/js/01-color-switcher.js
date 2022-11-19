2123bbfa42c1bce478491f50b62f2ca22fcd5045
Tetiana Kornilova <ktianaa@gmail.com>
2022-11-19 21:33:48 +0200

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', onStartClick);
+function onStartClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+}
+
+stopBtn.addEventListener('click', onStopClick);
+function onStopClick() {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+}
