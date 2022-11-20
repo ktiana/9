24df571cad34a1d1740e2b687a0d5db1eb78ab28
Tetiana Kornilova <ktianaa@gmail.com>
2022-11-20 17:48:40 +0200

    add timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,64 +1,54 @@
-//  Напиши скрипт таймера, який здійснює зворотний відлік до певної дати.
-//  Такий таймер може використовуватися у блогах та інтернет - магазинах, сторінках реєстрації подій,
-//     під час технічного обслуговування тощо.
-
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const startBtnEl = document.querySelector('button[data-start]');
-const inputEl = document.querySelector('#datetime-picker');
-startBtnEl.addEventListener('click', onClickStart);
-startBtnEl.setAttribute('disabled', true);
-let intervalId = null;
-let calendarDate = null;
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const pageValue = document.querySelectorAll('.value');
 
-function onClickStart() {
-  startBtnEl.setAttribute('disabled', true);
-  inputEl.setAttribute('disabled', true);
-  intervalId = setInterval(() => {
-    const diff = calendarDate - Date.now();
-
-    if (diff <= 0) {
-      clearInterval(intervalId);
-      return;
-    }
-
-    const { days, hours, minutes, seconds } = convertMs(diff);
-
-    document.querySelector('[data-days]').textContent = addLeadingZero(days);
-    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
-    document.querySelector('[data-minutes]').textContent =
-      addLeadingZero(minutes);
-    document.querySelector('[data-seconds]').textContent =
-      addLeadingZero(seconds);
-  }, 1000);
-}
+startBtn.disabled = true;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-
   onClose(selectedDates) {
-    calendarDate = selectedDates[0];
-
-    if (calendarDate < new Date()) {
-      Notiflix.Notify.failure('Please choose a date in the future', {
-        clickToClose: true,
-        timeout: 2000,
-      });
-      startBtnEl.setAttribute('disabled', true);
-      return;
-    } else {
-      startBtnEl.removeAttribute('disabled');
-      inputEl.setAttribute('disabled', true);
-      console.log(calendarDate);
-      clearInterval(intervalId);
-    }
+    const { defaultDate } = options;
+    const selectedDate = selectedDates[0];
+    let deltaTime = selectedDate.getTime() - defaultDate.getTime();
+    startTimer(deltaTime);
   },
 };
+
+flatpickr(input, options);
+
+function startTimer(deltaTime) {
+  if (deltaTime < 0) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    return;
+  }
+
+  startBtn.disabled = false;
+  startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    input.disabled = true;
+    const intervalId = setInterval(() => {
+      deltaTime -= 1000;
+      if (deltaTime <= 0) {
+        clearInterval(intervalId);
+        return;
+      }
+      const convertTime = convertMs(deltaTime);
+
+      const convertTimeArray = Object.entries(convertTime);
+      convertTimeArray.forEach((el, index) => {
+        const [, value] = el;
+        pageValue[index].textContent = value;
+      });
+    }, 1000);
+  });
+}
 
 function convertMs(ms) {
   const second = 1000;
@@ -66,10 +56,12 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
@@ -77,5 +69,3 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
-flatpickr(inputEl, options);
